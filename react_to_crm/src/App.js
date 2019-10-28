e64b3b6be3c0c8765f20babd9f075292e6be940a
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-10-28 12:31:29 -0700

    trying to fix app render issue

---
+++ react_to_crm/src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor (){
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+  componentDidMount() {
+    let projectsURL = ""
+  }
+render () {
+
+  return (
+    <div className="App">
+      <h1>Test wordpress react</h1>
+    </div>
+  );
+}
+}
+
+export default App;
